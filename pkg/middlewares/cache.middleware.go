dce893beee96781afa1dfed3c003024f6af81391
Vinicius Branco <viniblima2016@gmail.com>
2025-01-21 16:12:03 -0300

    feat: :sparkles: add middleware to validate wallet's store
    adding middleware to validate wallet of store with headers

--- pkg/middlewares/cache.middleware.go
+++ pkg/middlewares/cache.middleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
@@ -17,7 +18,8 @@ type cacheMiddleware struct {
 }
 
 type CacheMiddleware interface {
-	VerifyWallet(ctx *fiber.Ctx) error
+	VerifyUserWallet(ctx *fiber.Ctx) error
+	VerifyStoreWallet(ctx *fiber.Ctx) error
 }
 
 func NewCacheMiddleware(redisClient redis.Client, contextLocal context.Context) CacheMiddleware {
@@ -27,7 +29,63 @@ func NewCacheMiddleware(redisClient redis.Client, contextLocal context.Context) 
 	}
 }
 
-func (m *cacheMiddleware) VerifyWallet(ctx *fiber.Ctx) error {
+func (m *cacheMiddleware) VerifyStoreWallet(ctx *fiber.Ctx) error {
+	sID := ctx.Get("Store")
+
+	if sID != "" {
+		split := strings.Split(sID, "key=")
+
+		if len(split) < 2 {
+			return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Invalid tag",
+			})
+		}
+
+		userID := ctx.Locals("userID").(string)
+		list, err := m.redisClient.Get(m.contextLocal, enums.CacheListUserWallet).Result()
+
+		if err != nil {
+			return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": "error on get wallet list",
+			})
+		}
+
+		obj := []byte(list)
+		listWallet := []models.Wallet{}
+
+		err = json.Unmarshal(obj, &listWallet)
+
+		if err != nil {
+			return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": "error on parse list",
+			})
+		}
+
+		filtered := models.Wallet{}
+		found := false
+		for _, item := range listWallet {
+			if item.User == userID && item.ID == split[1] {
+				filtered = item
+				found = true
+				break
+			}
+		}
+
+		if found {
+			ctx.Locals("walletID", filtered.ID)
+			return ctx.Next()
+		} else {
+			return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"error": "User do not have a wallet",
+			})
+		}
+	}
+	return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
+		"error": "Invalid headers",
+	})
+}
+
+func (m *cacheMiddleware) VerifyUserWallet(ctx *fiber.Ctx) error {
 
 	userID := ctx.Locals("userID").(string)
 	list, err := m.redisClient.Get(m.contextLocal, enums.CacheListUserWallet).Result()
@@ -49,14 +107,18 @@ func (m *cacheMiddleware) VerifyWallet(ctx *fiber.Ctx) error {
 		})
 	}
 
-	filtered := []models.Wallet{}
+	filtered := models.Wallet{}
+	found := false
 	for _, item := range listWallet {
 		if item.User == userID {
-			filtered = append(filtered, item)
+			filtered = item
+			found = true
+			break
 		}
 	}
 
-	if len(filtered) > 0 {
+	if found {
+		ctx.Locals("walletID", filtered.ID)
 		return ctx.Next()
 	} else {
 		return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
